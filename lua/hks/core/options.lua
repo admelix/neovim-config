58cb3450d10bb6d341595ffdb7629853c9987f0a
jose <hks@joses-MacBook-Air.local>
2024-03-19 22:49:01 -0500

    se crea el repositorio de neovim

---
+++ lua/hks/core/options.lua
@@ -0,0 +1,38 @@
+-- for conciseness
+local opt = vim.opt 
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tab & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+--clipboard
+opt.clipboard:append("unnamedplus")
+
+--split window
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
+
+
